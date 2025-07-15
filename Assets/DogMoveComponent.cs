4b31b9e8ba1d5bd14f8a26eda3ac2a72446ebfc1
alxalx111 <elm.g2016@yandex.ru>
2025-07-15 12:54:49 +0300

    dz_1

--- Assets/DogMoveComponent.cs
+++ Assets/DogMoveComponent.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class DogMoveComponent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float moveSpeed = 1f;
 }
